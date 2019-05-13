79144294e48eef98aa04144bff97710d4a74d4ff
Huttman Balázs <huba95@hotmail.com>
2019-05-13 22:11:24 +0200

    Main finished

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -4,7 +4,10 @@
 
 int main()
 {
-
+	std::string number{};
+	std::cout << "Give me a Roman number: ";
+	std::cin >> number;
+	std::cout << std::endl << "The Arabic format of this number is: ";
 
 	system("pause");
 	return 0;
